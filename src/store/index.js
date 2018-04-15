a3a0aaafa111defce56c2a7503e1b9f90786b3a0
Dinesh Ramasamy <dinesh.vgp@gmail.com>
2018-04-15 17:40:46 +0530

    add redux-saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
-import rootReducer from 'reducers';
+import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import rootReducer from 'reducers';
+// import rootSaga from 'sagas';
 
 export const history = createHistory();
+const sagaMiddleware = createSagaMiddleware();
 
 const enhancers = [];
-const middlewares = [routerMiddleware(history)];
+const middlewares = [routerMiddleware(history), sagaMiddleware];
 
 const composedEnhancers = composeWithDevTools(
   applyMiddleware(...middlewares),
@@ -15,5 +18,7 @@ const composedEnhancers = composeWithDevTools(
 );
 
 const store = createStore(rootReducer, composedEnhancers);
+
+// sagaMiddleware.run(rootSaga);
 
 export default store;
