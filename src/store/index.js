ac426e5d295859b57ffd4e79f045d0eacf9f7a35
Dinesh Ramasamy <dinesh.vgp@gmail.com>
2018-04-15 18:16:18 +0530

    install and setup redux

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware } from "redux";
+import { routerMiddleware } from "react-router-redux";
+import createHistory from "history/createBrowserHistory";
+import rootReducer from "reducers";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+
+export const history = createHistory();
+
+const enhancers = [];
+const middlewares = [routerMiddleware(history)];
+
+const composedEnhancers = composeWithDevTools(
+  applyMiddleware(...middlewares),
+  ...enhancers
+);
+
+const store = createStore(rootReducer, composedEnhancers);
+
+export default store;
